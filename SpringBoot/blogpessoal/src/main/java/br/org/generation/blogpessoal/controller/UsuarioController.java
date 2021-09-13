2e801198d06dee3790fa9811adce6b6991b654a2
Felipe Fernandes <fjuanlobo@hotmail.com>
2021-09-13 19:29:10 -0300

    Atualização

--- SpringBoot/blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
+++ SpringBoot/blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
@@ -1,11 +1,13 @@
 package br.org.generation.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +25,11 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@GetMapping("/all")
+	public ResponseEntity <List<Usuario>> getAll(){
+		return ResponseEntity.ok(usuarioService.listarUsuarios());
+	}
 
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> login(@RequestBody Optional<UsuarioLogin> user) {
