f67bd54b9675f0fdc68e8e50ca1acf2f15365845
HellLobo <fjuanlobo@hotmail.com>
2021-09-30 22:51:47 -0300

    Atualizando blogPessoal

--- BlogPessoal/blogPessoal/src/app/service/auth.service.ts
+++ BlogPessoal/blogPessoal/src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,16 @@ export class AuthService {
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar',user)
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 
 }
